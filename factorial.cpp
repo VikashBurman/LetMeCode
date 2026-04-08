785d8ba13eafc32cb339123f200c4222f145a776
Vikash Burman <vikasburman37@gmail.com>
2026-04-08 20:18:18 +0530

    factorial ques

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int fact = 1;
+    for(int i=n;i>0;i--){
+        fact = fact*i;
+    }
+    cout<<fact<<endl;
+    return 0;
+}
