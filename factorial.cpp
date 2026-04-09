7a7737eebb6a9abc914f0f03fd7a8cf05677de21
Vikash Burman <vikasburman37@gmail.com>
2026-04-09 20:18:04 +0530

    Prime 2ques

--- factorial.cpp
+++ factorial.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
-    int fact = 1;
-    for(int i=n;i>0;i--){
+    int n;cin>>n;
+    if(n<1){
+        cout<<"No fact of negative"<<endl;
+        return 0;
+    }
+    long long fact = 1;
+    for(int i=1;i<=n;i++){
         fact = fact*i;
     }
     cout<<fact<<endl;
---
+++ primeNum.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    if(n == 0 || n == 1){
+        cout<<"Not Prime"<<endl;
+        return 0;
+    }
+    // for(int i=2;i<n;i++){
+    //     if(n%i == 0){
+    //         cout<<"Not Prime"<<endl;
+    //         return 0;
+    //     }
+    // }
+    for(int i=2;i*i<=n;i++){
+        if(n%i == 0){
+            cout<<"Not Prime"<<endl;
+            return 0;
+        }
+    }
+    cout<<"Prime"<<endl;
+    return 0;
+}
